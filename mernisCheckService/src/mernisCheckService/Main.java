0b826ab943c994b725edc0b9802a89a8cfbe549e
safalifter <safalifter@localhost>
2022-03-06 13:17:29 +0300

    uploaded with eclipse-git

---
+++ mernisCheckService/src/mernisCheckService/Main.java
@@ -0,0 +1,23 @@
+package mernisCheckService;
+
+import java.rmi.RemoteException;
+
+import Concrete.BaseCustomerManager;
+import Concrete.CustomerCheckManager;
+import Concrete.NeroCustomerManager;
+import Concrete.StarbucksCustomerManager;
+import Entities.Customer;
+
+public class Main {
+
+	public static void main(String[] args) throws RemoteException {
+
+		BaseCustomerManager customerManager1 = new NeroCustomerManager();
+
+		customerManager1.save(new Customer(1, "Safa", "Yesilyurt", "2001", "11111111111"));
+
+		BaseCustomerManager customerManager2 = new StarbucksCustomerManager(new CustomerCheckManager());
+
+		customerManager2.save(new Customer(1, "Safa", "Yesilyurt", "2001", "11111111111"));
+	}
+}
